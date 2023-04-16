20b9c7f2e8d1ca7d5e39a83117387068f78fbc23
unknown <ma4545@fayoum.edu.eg>
2023-04-16 20:56:21 +0200

    task6

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,156 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          backgroundColor: const Color.fromRGBO(0, 150, 136, 1),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage("images/profile.jpg"),
-              ),
-              const Text(
-                'MOHAMED FARAG',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 20.0,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'FLUTTER DEVELOPER',
-                style: TextStyle(
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: const Icon(
-                    Icons.phone,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    '+20 1000000000',
-                    style: TextStyle(
-                      color: Colors.teal.shade900, 
-                      fontSize: 20.0,
+  runApp(const BasketBall());
+}
+
+class BasketBall extends StatefulWidget {
+  const BasketBall({Key? key}) : super(key: key);
+
+  @override
+  State<BasketBall> createState() => _BasketBallState();
+}
+
+class _BasketBallState extends State<BasketBall> {
+  int teamAPoints = 0;
+  int teamBPoints = 0;
+  
+
+  void reset() {
+    setState(() {
+      teamAPoints = 0;
+      teamBPoints = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Basketball Score Counter'),
+            ),
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        const Text('Team A', style: TextStyle(fontSize: 30)),
+                        Text('$teamAPoints',
+                            style: const TextStyle(fontSize: 100)),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                    
+                                      setState(() {
+                                        teamAPoints += 1;
+                                      });
+                                    
+                                  },
+                                child: const Text('+1 Point',
+                                    style: TextStyle(fontSize: 20))),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                      setState(() {
+                                        teamAPoints += 2;
+                                      });
+                                  }
+                                child: const Text('+2 Points',
+                                    style: TextStyle(fontSize: 20))),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                      setState(() {
+                                        teamAPoints += 3;
+                                      });
+                                  }
+                                child: const Text('+3 Points',
+                                    style: TextStyle(fontSize: 20))),
+                          ],
+                        ),
+                      ],
                     ),
-                  ),
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: ListTile(
-                  leading: const Icon(
-                    Icons.email,
-                    color: Colors.teal,
-                  ),
-                  title: Text(
-                    'mohamed@gmail.com',
-                    style: TextStyle(
-                      color: Colors.teal.shade900, 
-                      fontSize: 20.0,
+                    const SizedBox(
+                      height: 400,
+                      child: VerticalDivider(
+                        thickness: 2,
+                      ),
                     ),
-                  ),
+                    Column(
+                      children: [
+                        const Text('Team B', style: TextStyle(fontSize: 30)),
+                        Text('$teamBPoints',
+                            style: const TextStyle(fontSize: 100)),
+                        Column(
+                          children: [
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                    
+                                      setState(() {
+                                        teamBPoints += 1;
+                                      });
+                                  
+                                  },
+                                child: const Text('+1 Point',
+                                    style: TextStyle(fontSize: 20))),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                    
+                                      setState(() {
+                                        teamBPoints += 2;
+                                      });
+                                    
+                                  },
+                                child: const Text('+2 Points',
+                                    style: TextStyle(fontSize: 20))),
+                            ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: const Size(120, 40),
+                                ),
+                                onPressed: () => () {
+                                    
+                                      setState(() {
+                                        teamBPoints += 3;
+                                      });
+                
+                                  }
+                                child: const Text('+3 Points',
+                                    style: TextStyle(fontSize: 20))),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-              ),
+                // reset button
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(120, 40),
+                      backgroundColor: Colors.grey[300],
+                    ),
+                    onPressed: () => reset(),
+                    child: const Text('Reset',
+                        style: TextStyle(color: Colors.black)))
               ],
-          )),
-    ),
-  );
+            )));
+  }
 }
